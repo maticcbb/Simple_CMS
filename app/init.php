771d801e3510a2e394826ddc066a406af0389795
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2018-11-10 12:45:11 +0100

    Functions for databases and URLs

--- app/init.php
+++ app/init.php
@@ -1,10 +1,14 @@
 <?php
 
-include("models/m_template.php");
-include("models/m_auth.php");
+define("SITE_PATH", 'http://localhost:8000/CMS_projekt/');
+define("APP_PATH", str_replace("\\" , "/" , dirname(__FILE__)) . "/");
+define("SITE_RES", 'http://localhost:8000/CMS_projekt/app/res');
 
-$Template = new Template();
-$Template->setAlertTypes(array('success','warning','error'));
-$Auth = new Auth();
+/* ustawienia bazy danych */
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'cms_projekt';
 
-session_start();
+require_once(APP_PATH."core/core.php");
+$SK = new SK_Core($server,$user,$password,$db);
